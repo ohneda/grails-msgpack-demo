9d55239d033c1379d9b1d8d8c9c7ca02b1ba4954
ohneda <ohneda@gmail.com>
2011-07-19 23:03:07 -0700

    added service methods, and controller action for JSON

--- grails-app/controllers/org/grails/msgpack/demo/MessageController.groovy
+++ grails-app/controllers/org/grails/msgpack/demo/MessageController.groovy
@@ -5,9 +5,48 @@ import grails.converters.JSON;
 class MessageController {
 
     def messageService
+
     def show = {
         def message = messageService.get(params.int('id'))
         assert message
         render message as JSON
+    }
+
+    def save = {
+
+        def jsonObject = request.JSON
+
+        def comments = jsonObject.comments
+        jsonObject.comments = null
+
+        def message = new Message( jsonObject )
+        message.props = jsonObject.props
+
+        message.comments =[]
+        comments.each{ c->
+            def comment = new Comment(c)
+            comment.message = message
+            message.addToComments(comment)
+        }
+
+        def result = messageService.save(message)
+        render result as JSON
+    }
+
+    def update = {
+        def jsonObject = request.JSON
+
+        def message = new Message( jsonObject )
+        message.id = params.int('id')
+        message.props = jsonObject.props
+
+        def result = messageService.update(message)
+        render result as JSON
+    }
+
+    def delete = {
+        messageService.delete(params.int('id'))
+        def result = [id: "${params.id}"]
+        render result as JSON
     }
 }
--- grails-app/services/org/grails/msgpack/demo/MessageService.groovy
+++ grails-app/services/org/grails/msgpack/demo/MessageService.groovy
@@ -8,7 +8,43 @@ class MessageService {
     Message get(Long id) {
         assert id
         def result = Message.get(id)
-        log.debug("get: ${result.dump()}")
+        log.debug("get message: ${result.dump()}")
         result
     }
+
+    Message save(Message message){
+        log.debug("save message: ${message?.dump()}")
+        message.dateCreated = new Date()
+        message.dateUpdated = new Date()
+        saveOrUpdate(message)
+    }
+
+    Message update(Message message){
+        log.debug("update message: ${message?.dump()}")
+        def target = Message.get(message.id)
+        def comments = target.comments
+        def dateCreated = target.dateCreated
+        target.properties = message.properties
+
+        target.comments = comments
+        target.dateCreated = dateCreated
+        target.dateUpdated = new Date()
+        saveOrUpdate(target)
+    }
+
+    Long delete(Long id){
+        log.debug("delete message: ${id}")
+        def target = Message.get(id)
+        log.debug(target?.dump() )
+        target.delete()
+        id
+    }
+
+    Message saveOrUpdate(Message message){
+        log.debug("saveOrUpdate: ${message?.dump()}")
+        message.save()
+        log.debug(message.errors)
+        message
+    }
+
 }
